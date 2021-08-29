98f1c175ec243b4c0f639299cea8303947968625
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-08-29 01:58:59 -0300

    update. question 1 complete! ♥

--- ParseHexInt/parseHexInt.java
+++ ParseHexInt/parseHexInt.java
@@ -1,30 +1,13 @@
 package ParseHexInt;
-import java.io.*;
 
 public class parseHexInt{
     public static void main(String args[]){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String entrada = "";
-        System.out.println("[CONSOLE INICIADO]");
-        System.out.println("[EXIT = sair]");
-        System.out.println("Insira um padrão hexadecimal para converte-lo em decimal:");
-        do {
-            try {
-                entrada = br.readLine();
-                if (!entrada.isBlank() && !entrada.isEmpty() && !entrada.equals("sair")){
-                    if (convertoHexToDecimal(entrada) < 0) {
-                        System.out.println(">erro: insira numeros de 0-9 positivos e letras de A-F sem espaços entre os mesmos");
-                    } else{
-                        System.out.println(">" + convertoHexToDecimal(entrada));
-                    }
-                }
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
-        } while(!entrada.equals("sair"));
-        System.out.println("[CONSOLE ENCERRADO]");
+        System.out.println(convertHexToDecimal("a"));
+        System.out.println(convertHexToDecimal("10"));
+        System.out.println(convertHexToDecimal("abc"));
+        System.out.println(convertHexToDecimal("f1de"));
     }
-    public static int convertoHexToDecimal(String hexa){
+    public static int convertHexToDecimal(String hexa){
         String chars = "0123456789ABCDEF";
         hexa = hexa.toUpperCase();
         int val = 0;
@@ -37,15 +20,35 @@ public class parseHexInt{
         return val;
     }
 }
-// // SIMPLE VERSION
+
+// // BUFFER VERSION
+// package ParseHexInt;
 // import java.io.*;
 
 // public class parseHexInt{
 //     public static void main(String args[]){
-//         System.out.println(convertHexToDecimal("a"));
-//         System.out.println(convertHexToDecimal("10"));
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         String entrada = "";
+//         System.out.println("[CONSOLE INICIADO]");
+//         System.out.println("[EXIT = sair]");
+//         System.out.println("Insira um padrão hexadecimal para converte-lo em decimal:");
+//         do {
+//             try {
+//                 entrada = br.readLine();
+//                 if (!entrada.isBlank() && !entrada.isEmpty() && !entrada.equals("sair")){
+//                     if (convertoHexToDecimal(entrada) < 0) {
+//                         System.out.println(">erro: insira numeros de 0-9 positivos e letras de A-F sem espaços entre os mesmos");
+//                     } else{
+//                         System.out.println(">" + convertoHexToDecimal(entrada));
+//                     }
+//                 }
+//             }catch (IOException e) {
+//                 e.printStackTrace();
+//             }
+//         } while(!entrada.equals("sair"));
+//         System.out.println("[CONSOLE ENCERRADO]");
 //     }
-//     public static int convertHexToDecimal(String hexa){
+//     public static int convertoHexToDecimal(String hexa){
 //         String chars = "0123456789ABCDEF";
 //         hexa = hexa.toUpperCase();
 //         int val = 0;
