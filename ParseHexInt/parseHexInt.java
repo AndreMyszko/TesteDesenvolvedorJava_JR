70236687f6554742b40dda109841f50df6d480e5
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-08-28 23:24:45 -0300

    update. question 1 complete! ♥

--- ParseHexInt/parseHexInt.java
+++ ParseHexInt/parseHexInt.java
@@ -37,3 +37,24 @@ public class parseHexInt{
         return val;
     }
 }
+// // SIMPLE VERSION
+// import java.io.*;
+
+// public class parseHexInt{
+//     public static void main(String args[]){
+//         System.out.println(convertHexToDecimal("a"));
+//         System.out.println(convertHexToDecimal("10"));
+//     }
+//     public static int convertHexToDecimal(String hexa){
+//         String chars = "0123456789ABCDEF";
+//         hexa = hexa.toUpperCase();
+//         int val = 0;
+//         for (int i = 0; i < hexa.length(); i++)
+//         {
+//             char charPosition = hexa.charAt(i);
+//             int charValue = chars.indexOf(charPosition);
+//             val = 16*val + charValue;
+//         }
+//         return val;
+//     }
+// }
